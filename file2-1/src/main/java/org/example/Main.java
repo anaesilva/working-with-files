dd2722da2a07877937ff2dc8fa216c68f26f41b8
anaesilva <annadasilva89@hotmail.com>
2024-07-19 18:44:56 -0300

    Bloco try-with-resources

---
+++ file2-1/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String path = "C:\\temp\\in.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
