f578fe3fc2e9620e3d212b41f4693fb1e84502a9
anaesilva <annadasilva89@hotmail.com>
2024-07-19 18:56:21 -0300

    FileWriter e BufferedWriter

---
+++ file3/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
+        String path = "C:\\temp\\out.txt";
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
